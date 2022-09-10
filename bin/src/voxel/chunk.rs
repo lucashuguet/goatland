98a47ed8cc08de4016de7482a76becfa14686ee8
Lucas Huguet <lucashuguet31@gmail.com>
2022-09-10 23:26:36 +0200

    terrain gen using block-mesh and OpenSimplex noise

---
+++ bin/src/voxel/chunk.rs
@@ -0,0 +1,63 @@
+use block_mesh::ndshape::{ConstShape, ConstShape3u32};
+use block_mesh::{
+    visible_block_faces, MergeVoxel, UnitQuadBuffer, Voxel, VoxelVisibility,
+    RIGHT_HANDED_Y_UP_CONFIG,
+};
+
+use noise::{NoiseFn, OpenSimplex};
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+struct BoolVoxel(bool);
+
+const EMPTY: BoolVoxel = BoolVoxel(false);
+const FULL: BoolVoxel = BoolVoxel(true);
+
+impl Voxel for BoolVoxel {
+    fn get_visibility(&self) -> VoxelVisibility {
+        if *self == EMPTY {
+            VoxelVisibility::Empty
+        } else {
+            VoxelVisibility::Opaque
+        }
+    }
+}
+
+impl MergeVoxel for BoolVoxel {
+    type MergeValue = Self;
+
+    fn merge_value(&self) -> Self::MergeValue {
+        *self
+    }
+}
+
+pub fn genchunk(chunkx: i32, chunkz: i32) -> UnitQuadBuffer {
+    let noise = OpenSimplex::new();
+
+    type ChunkShape = ConstShape3u32<18, 130, 18>;
+
+    let mut voxels = [FULL; ChunkShape::SIZE as usize];
+
+    for i in 0..ChunkShape::SIZE {
+        let [x, y, z] = ChunkShape::delinearize(i);
+
+        let noisey = (noise.get([
+            (x as i32 + chunkx * 16) as f64 / 10.,
+            (z as i32 + chunkz * 16) as f64 / 10.,
+        ]) * 10.) as u32
+            + 10;
+
+        voxels[i as usize] = if y <= noisey { FULL } else { EMPTY }
+    }
+
+    let mut buffer = UnitQuadBuffer::new();
+    visible_block_faces(
+        &voxels,
+        &ChunkShape {},
+        [0; 3],
+        [17, 129, 17],
+        &RIGHT_HANDED_Y_UP_CONFIG.faces,
+        &mut buffer,
+    );
+
+    buffer
+}
--- bin/src/voxel/terrain.rs
+++ bin/src/voxel/terrain.rs
@@ -1,35 +1,124 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
-use block_mesh::ndshape::{ConstShape, ConstShape3u32};
-use block_mesh::{
-    greedy_quads, GreedyQuadsBuffer, MergeVoxel, Voxel, VoxelVisibility, RIGHT_HANDED_Y_UP_CONFIG,
-};
-use noise::{NoiseFn, OpenSimplex};
+use block_mesh::RIGHT_HANDED_Y_UP_CONFIG;
+
+use super::chunk;
+
+#[derive(Component)]
+pub struct Chunk {
+    #[allow(dead_code)]
+    position: Vec2,
+}
 
 #[derive(Component)]
 pub struct TerrainGen;
 
-#[derive(Clone, Copy, Eq, PartialEq)]
-struct BoolVoxel(bool);
+#[derive(Debug)]
+enum Orientation {
+    Top,
+    Bottom,
+    Left,
+    Right,
+    Back,
+    Front,
+}
 
-const EMPTY: BoolVoxel = BoolVoxel(false);
-const FULL: BoolVoxel = BoolVoxel(true);
+fn deg2rand(deg: i32) -> f32 {
+    deg as f32 * PI / 180.
+}
 
-impl Voxel for BoolVoxel {
-    fn get_visibility(&self) -> VoxelVisibility {
-        if *self == EMPTY {
-            VoxelVisibility::Empty
-        } else {
-            VoxelVisibility::Opaque
-        }
+fn squaregen(
+    size: Vec2,
+    minimum: Vec3,
+    orientation: &Orientation,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    material: Handle<StandardMaterial>,
+) -> PbrBundle {
+    let putorigin2corner: Vec3 = Vec3::new(size.x / 2. - 0.5, size.y / 2. - 0.5, 0.5);
+
+    let (position, rotation) = match orientation {
+        Orientation::Top => (
+            minimum + Vec3::new(0., size.y / 2., -size.y / 2.) + putorigin2corner,
+            Quat::from_rotation_x(deg2rand(-90)),
+        ),
+        Orientation::Bottom => (
+            minimum + Vec3::new(0., -size.y / 2., size.y / 2. - 1.) + putorigin2corner,
+            Quat::from_rotation_x(deg2rand(90)),
+        ),
+        Orientation::Left => (
+            minimum + Vec3::new(-size.x / 2., 0., size.x / 2. - 1.) + putorigin2corner,
+            Quat::from_rotation_y(deg2rand(-90)),
+        ),
+        Orientation::Right => (
+            minimum + Vec3::new(-size.x / 2. + 1., 0., -size.x / 2.) + putorigin2corner,
+            Quat::from_rotation_y(deg2rand(90)),
+        ),
+        Orientation::Front => (
+            minimum + Vec3::new(0., 0., 0.) + putorigin2corner,
+            Quat::IDENTITY,
+        ),
+        Orientation::Back => (
+            minimum + Vec3::new(-size.x + 1., 0., -1.) + putorigin2corner,
+            Quat::from_rotation_y(deg2rand(180)),
+        ),
+    };
+
+    PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad { size, ..default() })),
+        material,
+        transform: Transform {
+            translation: position,
+            rotation,
+            ..default()
+        },
+        ..default()
     }
 }
 
-impl MergeVoxel for BoolVoxel {
-    type MergeValue = Self;
+pub fn spawnchunk(
+    commands: &mut Commands,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    coord: Vec2,
+) {
+    let buffer = chunk::genchunk(coord.x as i32, coord.y as i32);
 
-    fn merge_value(&self) -> Self::MergeValue {
-        *self
-    }
+    commands
+        .spawn_bundle(PbrBundle {
+            transform: Transform::from_xyz(coord.x * 16., 0., coord.y * 16.),
+            ..default()
+        })
+        .with_children(|parent| {
+            for (index, quadbuffer) in buffer.groups.iter().enumerate() {
+                let normal = RIGHT_HANDED_Y_UP_CONFIG.faces[index].quad_mesh_normals()[0];
+
+                let orientation = match [normal[0] as i32, normal[1] as i32, normal[2] as i32] {
+                    [1, 0, 0] => Orientation::Right,
+                    [0, 1, 0] => Orientation::Top,
+                    [0, 0, 1] => Orientation::Front,
+                    [-1, 0, 0] => Orientation::Left,
+                    [0, -1, 0] => Orientation::Bottom,
+                    [0, 0, -1] => Orientation::Back,
+                    _ => panic!("Invalid normal"),
+                };
+
+                for quad in quadbuffer.iter() {
+                    parent.spawn_bundle(squaregen(
+                        Vec2::new(1., 1.),
+                        Vec3::new(
+                            quad.minimum[0] as f32,
+                            quad.minimum[1] as f32,
+                            quad.minimum[2] as f32,
+                        ),
+                        &orientation,
+                        meshes,
+                        materials.add(Color::GREEN.into()),
+                    ));
+                }
+            }
+        })
+        .insert(Chunk { position: coord });
 }
 
 pub fn generation(
@@ -37,42 +126,92 @@ pub fn generation(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let noise = OpenSimplex::new();
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1. })),
+        material: materials.add(Color::YELLOW.into()),
+        transform: Transform::from_xyz(0.5, 0.5, 0.5),
+        ..default()
+    });
 
-    type ChunkShape = ConstShape3u32<16, 16, 16>;
+    // commands.spawn_bundle(squaregen(
+    //     Vec2::ONE,
+    //     Vec3::ZERO,
+    //     &Orientation::Front,
+    //     &mut meshes,
+    //     materials.add(Color::GREEN.into()),
+    // ));
 
-    let mut voxels = [EMPTY; ChunkShape::SIZE as usize];
+    // commands.spawn_bundle(squaregen(
+    //     Vec2::ONE,
+    //     Vec3::ZERO,
+    //     &Orientation::Back,
+    //     &mut meshes,
+    //     materials.add(Color::BLACK.into()),
+    // ));
 
-    for i in 0..ChunkShape::SIZE {
-        let [x, y, z] = ChunkShape::delinearize(i);
+    // commands.spawn_bundle(squaregen(
+    //     Vec2::ONE,
+    //     Vec3::ZERO,
+    //     &Orientation::Left,
+    //     &mut meshes,
+    //     materials.add(Color::BLUE.into()),
+    // ));
 
-        voxels[i as usize] = if y < noise.get([x as f64, z as f64]) as u32 {
-            FULL
-        } else {
-            EMPTY
-        }
-    }
+    // commands.spawn_bundle(squaregen(
+    //     Vec2::ONE,
+    //     Vec3::ZERO,
+    //     &Orientation::Right,
+    //     &mut meshes,
+    //     materials.add(Color::PINK.into()),
+    // ));
 
-    let mut buffer = GreedyQuadsBuffer::new(voxels.len());
-    greedy_quads(
-        &voxels,
-        &ChunkShape {},
-        [0; 3],
-        [16; 3],
-        &RIGHT_HANDED_Y_UP_CONFIG.faces,
-        &mut buffer,
+    // commands.spawn_bundle(squaregen(
+    //     Vec2::ONE,
+    //     Vec3::ZERO,
+    //     &Orientation::Top,
+    //     &mut meshes,
+    //     materials.add(Color::RED.into()),
+    // ));
+
+    // commands.spawn_bundle(squaregen(
+    //     Vec2::ONE,
+    //     Vec3::ZERO,
+    //     &Orientation::Bottom,
+    //     &mut meshes,
+    //     materials.add(Color::PURPLE.into()),
+    // ));
+
+    spawnchunk(
+        &mut commands,
+        &mut meshes,
+        &mut materials,
+        Vec2::new(0., 0.),
     );
 
-    commands
-        .spawn_bundle(PbrBundle {
-            transform: Transform::from_xyz(0., 0., 0.),
-            ..default()
-        })
-        .with_children(|parent| {});
+    spawnchunk(
+        &mut commands,
+        &mut meshes,
+        &mut materials,
+        Vec2::new(-1., 0.),
+    );
+
+    spawnchunk(
+        &mut commands,
+        &mut meshes,
+        &mut materials,
+        Vec2::new(-1., -1.),
+    );
+
+    spawnchunk(
+        &mut commands,
+        &mut meshes,
+        &mut materials,
+        Vec2::new(0., -1.),
+    )
 }
 
 impl Plugin for TerrainGen {
     fn build(&self, app: &mut App) {
-        app.add_system(generation);
+        app.add_startup_system(generation);
     }
 }
